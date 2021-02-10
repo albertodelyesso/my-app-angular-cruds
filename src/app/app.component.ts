217bfdd4d6d70f4eb4db50a84d49c1f5edeb26dd
albertodelyesso <alberto.delyesso@gmail.com>
2021-02-10 10:10:56 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EmptyError, from } from 'rxjs';
+
+import { Empleado } from './models/empleado';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Hola Mundo';
+
+  empleadosArray : Empleado[] = [
+  {id:1, nombre: "Ruby", pais: "Argentina"},
+  {id:2, nombre: "Perlita", pais: "Italia"},
+  {id:3, nombre: "Michu", pais: "Rusia"},
+  ];
+
+  selectedEmpleado: Empleado = new Empleado();
+
+  addOrEdit(){
+    if(this.selectedEmpleado.id === 0){
+      this.selectedEmpleado.id = this.empleadosArray.length + 1;
+      this.empleadosArray.push(this.selectedEmpleado);
+    }else{
+
+    }
+
+    this.selectedEmpleado = new Empleado();
+  }
+  
+  openForEdit(empleado: Empleado){
+    this.selectedEmpleado = empleado;
+  }
+
+  deleteEmpleado(empleado: Empleado){
+
+    if(confirm("Está seguro que desea eliminarlo? " + empleado.nombre + " id: " + empleado.id)){
+      const index = this.empleadosArray.findIndex(buscado => buscado.id === empleado.id); 
+      this.empleadosArray.splice(index, 1);
+    }
+    this.selectedEmpleado = new Empleado();
+  }
+
 }
